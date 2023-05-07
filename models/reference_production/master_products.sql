c76bc8e3087ffd1e87176aaf41c8264a42619166
LBaldassarre <lbaldassarre.dev@gmail.com>
2023-05-07 16:24:13 -0300

    Create master_products.sql model

---
+++ models/reference_production/master_products.sql
@@ -0,0 +1,21 @@
+{{ config(materialized='table') }}
+
+with source_products as (
+    select * from {{ source('reference_stage', 'products') }}
+),
+
+final as (
+    select 
+    productid,
+    left(product,charindex('|',product)-1) as product,
+    substr(product, charindex('|',product) + 1, length(product)) as subcategory,
+    case
+        when substr(product, charindex(' ',product) + 1, 1) = 'M' then 'Mix'
+        when substr(product, charindex(' ',product) + 1, 1) = 'R' then 'Rural'
+        when substr(product, charindex(' ',product) + 1, 1) = 'U' then 'Urban'
+        when substr(product, charindex(' ',product) + 1, 1) = 'Y' then 'Youth'
+    end as category
+    From source_products
+)
+
+select * from final
